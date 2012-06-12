d33fb5b61eef906fc6935ad5b4c9b1aeb267025c
Mani <sadhak001@gmail.com>
2012-06-12 19:28:26 +0100

    License header of scripts amended! Using format from scripts from PatchReview repo.

--- mani/cleanBuild.sh
+++ mani/cleanBuild.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # 
+# Originally incepted by Mike Barker and Martijn Verburg as wiki instructions for the participants of the OpenJDK hackathon sessions
+# Compiled into bash script files by Mani Sarkar.
+#
 # The GNU General Public License (GPL)
-
+#
 # Version 2, June 1991
 
 # Copyright (C) 1989, 1991 Free Software Foundation, Inc.
@@ -10,6 +13,26 @@
 # Everyone is permitted to copy and distribute verbatim copies of this license
 # document, but changing it is not allowed.
 # 
+# Copyright (c) 2012, Mani Sarkar <sadhak001@gmail.com>, Mike Barker <mike.barker@gmail.com>, Martijn Verburg <martijn.verburg@gmail.com> All rights reserved.
+# 
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+#
+# This code is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 only, as
+# published by the Free Software Foundation.  Oracle designates this
+# particular file as subject to the "Classpath" exception as provided
+# by Oracle in the LICENSE file that accompanied this code.
+#
+# This code is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# version 2 for more details (a copy is included in the LICENSE file that
+# accompanied this code).
+#
+# You should have received a copy of the GNU General Public License version
+# 2 along with this work; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+#
 
 cd $HOME/sources/jdk
 
